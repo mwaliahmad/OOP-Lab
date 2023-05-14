6e458b8f3e2367979c2ca2fbf7f9c06599b968a0
Muhammad Wali Ahmad <waliaslam2002@gmail.com>
2023-05-14 23:34:19 +0500

    Task

--- Week05/Challenge02/Challenge02/BL/Customer.cs
+++ Week05/Challenge02/Challenge02/BL/Customer.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Challenge02.BL
 {
-    class Customer
+    public class Customer
     {
+        public Customer(string name, string address, string contact)
+        {
+            this.name = name;
+            this.address = address;
+            this.contact = contact;
+            Products = new List<Product>();
+        }
+
+        public string name;
+        public string address;
+        public string contact;
+        public List<Product> Products;
+
+        public List<Product> GetProducts()
+        {
+            return Products;
+        }
+        public void AddProduct(Product P)
+        {
+            Products.Add(P);
+        }
+        public float GetInvoice()
+        {
+            float total = 0;
+            foreach (var P in Products)
+            {
+                total = total + P.price;
+            }
+            return total;
+        }
     }
 }
