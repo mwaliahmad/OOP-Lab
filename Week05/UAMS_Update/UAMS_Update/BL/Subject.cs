16eba8b02e9f46124a153d0b4459d081bf8a5251
Muhammad Wali Ahmad <waliaslam2002@gmail.com>
2023-05-14 15:49:43 +0500

    Task

---
+++ Week05/UAMS_Update/UAMS_Update/BL/Subject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAMS_Update.BL
+{
+    public class Subject
+    {
+        public Subject(int code, string type, int creditHours, float fees)
+        {
+            this.code = code;
+            this.type = type;
+            this.creditHours = creditHours;
+            this.fees = fees;
+        }
+
+        public int code;
+        public string type;
+        public int creditHours;
+        public float fees;
+
+        public int GetCreditHours()
+        {
+            return creditHours;
+        }
+    }
+}
