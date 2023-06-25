d4adbe6f2621abe8e498088c54561b532c3f5137
Muhammad Wali Ahmad <waliaslam2002@gmail.com>
2023-06-25 10:28:43 +0500

    Task

---
+++ Week10/Pacman/Pacman/GL/PacmanPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman.GL
+{
+    public class PacmanPlayer:GameObject
+    {
+        public PacmanPlayer(GameObjectType type, char displayCharacter) :base(type,displayCharacter)
+        {
+
+        }
+        public PacmanPlayer(char displayCharacter, GameCell cell) : base(displayCharacter,cell)
+        {
+
+        }
+        /*public GameCell move(GameDirection direction)
+        {
+            if(direction == GameDirection.Left)
+            {
+
+            }
+            else if(direction == GameDirection.Right)
+            {
+
+            }
+            else if( direction == GameDirection.Up)
+            {
+
+            }
+            else if (direction == GameDirection.Down)
+            {
+
+            }
+            else
+            {
+
+            }
+        }*/
+    }
+}
